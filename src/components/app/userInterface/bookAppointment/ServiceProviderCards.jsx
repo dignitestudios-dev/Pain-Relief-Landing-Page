989d0ce4ef34f9d9e48e9c2b9e913cf84552ddc6
steveparker123 <176498134+steveparker123@users.noreply.github.com>
2025-06-24 08:09:54 -0700

    Update: message

--- src/components/app/userInterface/bookAppointment/ServiceProviderCards.jsx
+++ src/components/app/userInterface/bookAppointment/ServiceProviderCards.jsx
@@ -6,73 +6,76 @@ const ServiceProviderCards = ({ providerData, loading }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-10 ">
+    <>
       {loading ? (
         <SkeletonProviderCard />
       ) : providerData.length === 0 ? (
-        <div className="flex relative top-0 left-20 bottom-0 right-0 justify-center items-center">
-          <EmptyScreen text={"No Search Results Found"} />
+        <div className="flex justify-center items-center w-full">
+          <EmptyScreen
+            text={
+              "No results match your filters. Try adjusting them to see more options"
+            }
+          />
         </div>
       ) : (
-        providerData?.map((item, index) => (
-          <div
-            key={index}
-            className="min-w-[250px] bg-white p-2 rounded-[14px] mx-2 w-[279px] flex flex-col justify-between min-h-[310px]"
-          >
-            <div className="bg-cards  h-24 flex items-center justify-center rounded-md">
-              <div className="bg-gradient-to-l from-[#29ABE2] to-[#63CFAC] p-[3px] rounded-full relative top-6  ">
-                <img
-                  src={item?.provider?.profilePicture}
-                  alt="Avatar"
-                  className="w-16 h-16 rounded-full bg-white"
-                />
-              </div>
-            </div>
-            <div className="text-center p-4">
-              <h3 className="text-[16px] font-[600] text-[#181818] ">
-                {item?.provider?.name}
-              </h3>
-              <p className="bg-gradient-to-l to-[#63CFAC] from-[#29ABE2] bg-clip-text text-transparent text-[14px] font-[500] ">
-                {item?.services?.flatMap((item) => item?.name).join(", ")}
-              </p>
-              <div className="flex  justify-center  gap-2">
-                <div>
+        <div className="grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-10 ">
+          {providerData?.map((item, index) => (
+            <div
+              key={index}
+              className="min-w-[250px] bg-white p-2 rounded-[14px] mx-2 w-[279px] flex flex-col justify-between min-h-[310px]"
+            >
+              <div className="bg-cards  h-24 flex items-center justify-center rounded-md">
+                <div className="bg-gradient-to-l from-[#29ABE2] to-[#63CFAC] p-[3px] rounded-full relative top-6  ">
                   <img
-                    src={Location}
-                    className="w-[13px] mt-1 h-[15.69px] object-contain  "
-                    alt=""
+                    src={item?.provider?.profilePicture}
+                    alt="Avatar"
+                    className="w-16 h-16 rounded-full bg-white"
                   />
                 </div>
-                <div className="text-[#181818] break-words whitespace-normal text-center text-[14px]  font-[500] ">
-                  {" "}
-                  {item?.address}
-                </div>
               </div>
-              <p className="text-[#565656] text-[14px] font-[500] mt-1">
-              {item?.distanceInMiles} Miles Away
-              </p>
-            </div>
-            <div className="flex justify-center items-center">
-              <div className="w-[200px]  relative top-5 ">
-                <Button
-                  text={"Book Now"}
-                  onClick={() =>
-                    navigate(
-                      `/user/appointmentDetailSection/${item?._id}`,
-                      {
+              <div className="text-center p-4">
+                <h3 className="text-[16px] font-[600] text-[#181818] ">
+                  {item?.provider?.name}
+                </h3>
+                <p className="bg-gradient-to-l to-[#63CFAC] from-[#29ABE2] bg-clip-text text-transparent text-[14px] font-[500] ">
+                  {item?.services?.flatMap((item) => item?.name).join(", ")}
+                </p>
+                <div className="flex  justify-center  gap-2">
+                  <div>
+                    <img
+                      src={Location}
+                      className="w-[13px] mt-1 h-[15.69px] object-contain  "
+                      alt=""
+                    />
+                  </div>
+                  <div className="text-[#181818] break-words whitespace-normal text-center text-[14px]  font-[500] ">
+                    {" "}
+                    {item?.address}
+                  </div>
+                </div>
+                <p className="text-[#565656] text-[14px] font-[500] mt-1">
+                  {item?.distanceInMiles} Miles Away
+                </p>
+              </div>
+              <div className="flex justify-center items-center">
+                <div className="w-[200px]  relative top-5 ">
+                  <Button
+                    text={"Book Now"}
+                    onClick={() =>
+                      navigate(`/user/appointmentDetailSection/${item?._id}`, {
                         state: {
                           provider: item,
                         },
-                      }
-                    )
-                  }
-                />
+                      })
+                    }
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        ))
+          ))}
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
--- src/components/global/EmptyScreen.jsx
+++ src/components/global/EmptyScreen.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const EmptyScreen = ({text}) => {
+const EmptyScreen = ({ text }) => {
   return (
-    <div className='text-[16px] font-[500] text-gray-500 '>{text}</div>
-  )
-}
+    <div className=" text-[16px] font-[500] text-gray-500 text-nowrap">
+      {text}
+    </div>
+  );
+};
 
-export default EmptyScreen
+export default EmptyScreen;
