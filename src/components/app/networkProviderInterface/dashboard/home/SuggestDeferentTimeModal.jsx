a27f55e5ea7ce86a872fb0f19dfb6929d412b727
steveparker123 <176498134+steveparker123@users.noreply.github.com>
2025-07-03 03:59:14 -0700

    Update: time modal

--- src/components/app/networkProviderInterface/dashboard/home/SuggestDeferentTimeModal.jsx
+++ src/components/app/networkProviderInterface/dashboard/home/SuggestDeferentTimeModal.jsx
@@ -59,7 +59,7 @@ const SuggestDeferentTimeModal = ({
               "9:00 AM",
               "10:00 AM",
               "11:00 AM",
-              "12:00 AM",
+              "12:00 PM",
               "1:00 PM",
               "2:00 PM",
               "3:00 PM",
